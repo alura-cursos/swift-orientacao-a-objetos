69cf46c31c55fbbb693fa8f1277b9ee1a16f3458
Giovanna <giovannagmoeller@gmail.com>
2022-11-29 19:59:54 -0300

    Aula 2.2 - Super e Override

---
+++ Super-Override.playground/Contents.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+class Conta {
+  var cpf: String
+  var saldo: Double
+  
+  init(cpf: String) {
+    self.cpf = cpf
+    self.saldo = 0
+  }
+  
+  func sacar(_ valor: Double) {
+    if valor <= saldo {
+      self.saldo -= valor
+    }
+  }
+  
+  func depositar(_ valor: Double) {
+    if valor > 0 {
+      self.saldo += valor
+    }
+  }
+}
+
+let conta = Conta(cpf: "888888888888")
+conta.depositar(100)
+conta.sacar(50)
+conta.saldo
+
+class ContaCorrente: Conta {
+  var possuiEmprestimo: Bool = true
+  
+  func solicitarEmprestimo(valor: Double) {
+    print("O cliente com CPF \(cpf) está requisitando um empréstimo de R$\(valor).")
+    possuiEmprestimo = true
+    super.depositar(valor)
+  }
+  
+  override func sacar(_ valor: Double) {
+    if valor <= (saldo + 5) {
+      self.saldo -= (valor + 5)
+    }
+  }
+}
+
+let contaCorrente = ContaCorrente(cpf: "99999999999")
+contaCorrente.solicitarEmprestimo(valor: 1000)
+contaCorrente.saldo
+
+class ContaPoupanca: Conta {
+  var possuiCartaoDebito: Bool
+  
+  init(cpf: String, possuiCartaoDebito: Bool) {
+    self.possuiCartaoDebito = possuiCartaoDebito
+    super.init(cpf: cpf) // deve ser chamado
+  }
+  
+  func solicitarCartaoDebito() {
+    possuiCartaoDebito = true
+  }
+  
+  override func sacar(_ valor: Double) {
+    if valor <= (saldo + 10) {
+      self.saldo -= (valor + 10)
+    }
+  }
+  
+}
+
+let contaPoupanca = ContaPoupanca(cpf: "11111111111", possuiCartaoDebito: false)
+contaPoupanca.depositar(100)
+contaPoupanca.sacar(90)
+contaPoupanca.saldo
