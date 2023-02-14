0e8d20db8a2e6b55125f3449d5d17afde9f05113
Giovanna <giovannagmoeller@gmail.com>
2023-02-14 01:40:31 -0300

    aula 1.5 - as principais diferenças entre structs e classes

---
+++ Aula01.playground/Pages/Aula 1.5 - As principais diferenças entre structs e classes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+struct ContaCorrenteStruct {
+    var saldo = 0.0
+    var nome: String
+    
+    mutating func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+    
+    mutating func depositar(_ valor: Double) {
+        saldo += valor
+    }
+}
+
+var contaCorrenteStruct01 = ContaCorrenteStruct(nome: "Giovanna")
+// let contaCorrenteStruct01 = ContaCorrenteStruct(nome: "Giovanna")
+var contaCorrenteStruct02 = contaCorrenteStruct01
+contaCorrenteStruct01.depositar(100)
+contaCorrenteStruct02.depositar(50)
+print("Saldo da conta corrente struct 01 = \(contaCorrenteStruct01.saldo)")
+print("Saldo da conta corrente struct 02 = \(contaCorrenteStruct02.saldo)")
+
+class ContaCorrenteClass {
+    var saldo = 0.0
+    var nome: String
+    
+    func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+    
+    func depositar(_ valor: Double) {
+        saldo += valor
+    }
+    
+    init(nome: String) {
+        self.nome = nome
+    }
+}
+
+let contaCorrenteClasse01 = ContaCorrenteClass(nome: "Ana")
+let contaCorrenteClasse02 = contaCorrenteClasse01
+contaCorrenteClasse01.depositar(60)
+contaCorrenteClasse02.depositar(100)
+print("Saldo da conta corrente classe 01 = \(contaCorrenteClasse01.saldo)")
+print("Saldo da conta corrente classe 02 = \(contaCorrenteClasse02.saldo)")
+
+
+
+
+
+
+
